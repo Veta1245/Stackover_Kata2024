389c65ceec7444b55b76b390df6fe372fd214915
timur555@yandex.ru <rxl22jrn>
2023-10-20 13:30:21 +0300

    ref

--- src/main/java/com/javamentor/qa/platform/dao/abstracts/dto/UserDtoDao.java
+++ src/main/java/com/javamentor/qa/platform/dao/abstracts/dto/UserDtoDao.java
@@ -1,8 +1,5 @@
 package com.javamentor.qa.platform.dao.abstracts.dto;
 
-import com.javamentor.qa.platform.dao.abstracts.repository.ReadWriteDao;
-import com.javamentor.qa.platform.models.dto.UserDto;
-
-public interface UserDtoDao extends ReadWriteDao<UserDto, Long> {
+public interface UserDtoDao {
 
 }
--- src/main/java/com/javamentor/qa/platform/service/abstracts/dto/UserDtoService.java
+++ src/main/java/com/javamentor/qa/platform/service/abstracts/dto/UserDtoService.java
@@ -1,7 +1,4 @@
 package com.javamentor.qa.platform.service.abstracts.dto;
 
-import com.javamentor.qa.platform.models.dto.UserDto;
-import com.javamentor.qa.platform.service.abstracts.repository.ReadWriteService;
-
-public interface UserDtoService extends ReadWriteService<UserDto, Long> {
+public interface UserDtoService {
 }
