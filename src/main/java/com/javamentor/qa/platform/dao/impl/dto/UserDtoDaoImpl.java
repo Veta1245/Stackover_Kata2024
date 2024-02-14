a2cb2868a276136b48289f2735b22aad51883ed0
Mike <dev@76543210.ru>
2024-02-14 12:43:35 +0000

    Resolve "Разработать api, который возвращает пользователя по его id #17"

--- src/main/java/com/javamentor/qa/platform/dao/impl/dto/UserDtoDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/dto/UserDtoDaoImpl.java
@@ -1,10 +1,13 @@
 package com.javamentor.qa.platform.dao.impl.dto;
 
 import com.javamentor.qa.platform.dao.abstracts.dto.UserDtoDao;
+import com.javamentor.qa.platform.dao.util.SingleResultUtil;
+import com.javamentor.qa.platform.models.dto.UserDto;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.Optional;
 
 @Repository
 public class UserDtoDaoImpl implements UserDtoDao {
@@ -13,4 +16,32 @@ public class UserDtoDaoImpl implements UserDtoDao {
     private EntityManager entityManager;
 
 
+    @Override
+    public Optional<UserDto> getById(Long userId) {
+
+        return SingleResultUtil.getSingleResultOrNull(entityManager.createQuery(
+                        """
+                                    SELECT distinct new com.javamentor.qa.platform.models.dto.UserDto(
+                                        u.id,
+                                        u.email,
+                                        u.fullName,
+                                        u.imageLink,
+                                        u.city,
+                                        cast(sum(r.count) as long),
+                                        u.persistDateTime,
+                                        cast(
+                                             (
+                                                 (SELECT count (va) as vac  FROM VoteAnswer va WHERE va.user = u) +
+                                                 (SELECT count (vq) as vqc  FROM VoteQuestion vq WHERE vq.user = u)
+                                             )
+                                        as long)
+                                    )
+                                    FROM User u
+                                    left join Reputation r ON r.author = u 
+                                    WHERE u.id = :id
+                                    GROUP BY u.id
+                                """,
+                        UserDto.class)
+                .setParameter("id", userId));
+    }
 }
