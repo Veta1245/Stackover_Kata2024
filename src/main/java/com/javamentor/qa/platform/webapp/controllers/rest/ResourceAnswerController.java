d8f1e883f047d0f2dd337ee13a9589e03c416cb5
timur555@yandex.ru <rxl22jrn>
2024-02-19 15:49:28 +0300

    ref

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceAnswerController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ResourceAnswerController.java
@@ -1,14 +1,13 @@
 package com.javamentor.qa.platform.webapp.controllers.rest;
 
 import com.javamentor.qa.platform.models.entity.user.User;
-import com.javamentor.qa.platform.service.abstracts.model.AnswerService;
-import com.javamentor.qa.platform.service.abstracts.model.ReputationService;
 import com.javamentor.qa.platform.service.abstracts.model.UserService;
 import com.javamentor.qa.platform.service.abstracts.model.VoteAnswerService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,19 +19,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.EntityNotFoundException;
 
-
 @Tag(name = "ResourceAnswerController", description = "Контроллер для работы с ответами на вопросы")
 @Slf4j
 @RequestMapping("/api/user/question/{questionId}/answer")
+@AllArgsConstructor
 @RestController
 public class ResourceAnswerController {
     private final VoteAnswerService voteAnswerService;
     private final UserService userService;
-
-    public ResourceAnswerController(VoteAnswerService voteAnswerService, UserService userService) {
-        this.voteAnswerService = voteAnswerService;
-        this.userService = userService;
-    }
 
     @Operation(summary = "Уменьшение оценки ответа",
             description = "Голос за ответ на вопрос,уменьшает репутацию автора ответа: -5 к репутации"
