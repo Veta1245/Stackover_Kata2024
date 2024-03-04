a880c8e3873966d55fde39988b00b6450e6d6996
Rashid Magomedov <lodbrokragnar548@gmail.com>
2024-03-04 09:21:27 +0000

    Resolve "Разработать api который возвращает лист, содержащий топ-10 тегов #42"

--- src/main/java/com/javamentor/qa/platform/dao/impl/dto/TagDtoDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/dto/TagDtoDaoImpl.java
@@ -1,14 +1,17 @@
 package com.javamentor.qa.platform.dao.impl.dto;
 
 import com.javamentor.qa.platform.dao.abstracts.dto.TagDtoDao;
+import com.javamentor.qa.platform.models.dto.RelatedTagDto;
 import com.javamentor.qa.platform.models.dto.TagDto;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
+@Transactional
 public class TagDtoDaoImpl implements TagDtoDao {
 
     @PersistenceContext
@@ -17,19 +20,32 @@ public class TagDtoDaoImpl implements TagDtoDao {
     @Override
     public List<TagDto> getTop3TagsByUserId(Long userId) {
         return entityManager.createQuery("""
-                select new com.javamentor.qa.platform.models.dto.TagDto
-                (tag.id, tag.name, tag.description)
-                from Tag tag
-                join tag.questions as question
-                join question.answers as answers
-                join answers.user as user
-                join answers.voteAnswers as voteAnswers
-                where user.id = :userId
-                group by tag.id, tag.name, tag.description
-                order by sum(case when voteAnswers.voteType = 'UP' then 1 when voteAnswers.voteType = 'DOWN' then -1 else 0 end)desc""", TagDto.class)
+                        select new com.javamentor.qa.platform.models.dto.TagDto
+                        (tag.id, tag.name, tag.description)
+                        from Tag tag
+                        join tag.questions as question
+                        join question.answers as answers
+                        join answers.user as user
+                        join answers.voteAnswers as voteAnswers
+                        where user.id = :userId
+                        group by tag.id, tag.name, tag.description
+                        order by sum(case when voteAnswers.voteType = 'UP' then 1 when voteAnswers.voteType = 'DOWN' then -1 else 0 end)desc""", TagDto.class)
                 .setMaxResults(3)
                 .setParameter("userId", userId)
                 .getResultList();
     }
-}
 
+    @Override
+    public List<RelatedTagDto> getTopTags(){
+        return entityManager.createQuery("""
+                        select new com.javamentor.qa.platform.models.dto.RelatedTagDto
+                                 (tag.id, tag.name , count(tag))
+                                 from Tag tag
+                                 join tag.questions questions
+                                 group by tag.id , tag.name
+                                 order by count (questions) desc""", RelatedTagDto.class)
+                .setMaxResults(10)
+                .getResultList();
+
+    }
+}
